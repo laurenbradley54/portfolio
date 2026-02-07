d8c985356d9667cf8b076fdd1706f6281e3463a8
laurenbradley54 <laurenbradley54@gmail.com>
2026-02-07 09:07:11 -0800

    Edited the portfolio to not have a # of years at booz allen

--- src/App.jsx
+++ src/App.jsx
@@ -194,8 +194,8 @@ const AboutSection = () => {
                             Hello,
                             <br />
                             <br />
-                            I'm Lauren, a data scientist, technology enthusiast, lifelong learner, and aspiring leader with a passion for turning data into insight. For the past three years, I've worked as a Data Engineer at Booz Allen Hamilton, where I've specialized in ETL workflows and led data pipeline development for dashboard products using Databricks, PySpark, Python, and SQL.
-                            I earned my Bachelor of Science in Computational Modeling and Data Analytics from Virginia Tech in December 2021, with coursework in machine learning, mathematical modeling, algorithms, and data visualization. During my college years, I interned at Northwestern Mutual, gaining hands-on experience in both data engineering and data science from building scalable systems to exploring predictive models. 
+                            I'm Lauren, a data scientist, technology enthusiast, lifelong learner, and aspiring leader with a passion for turning data into insight. Currently, I am a Data Engineer at Booz Allen Hamilton, where I've specialized in ETL workflows and led data pipeline development for dashboard products using Databricks, PySpark, Python, and SQL.
+                            I earned my Bachelor of Science in Computational Modeling and Data Analytics from Virginia Tech in December 2021, with coursework in machine learning, mathematical modeling, data structures and algorithms, and data visualization. During my college years, I interned at Northwestern Mutual, gaining hands-on experience in both data engineering and data science from building scalable systems to exploring predictive models. 
                             Originally from Richmond, Virginia, I began my career in Virginia Beach, followed by a TDY in Stuttgart, Germany in 2024, and now based in San Diego, California. Outside of work, I enjoy traveling, surfing, and supporting the growth and empowerment of others.
                             Thanks for stopping by, I'd love to connect!
                             <br />
